a77ce250849599d2c4c961ff4296c8f4ccd49241
OkadaGenya <g.okada.wilf+github@gmail.com>
2018-04-15 19:36:46 +0900

    add feed

---
+++ src/components/Feed.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center>
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
+</template>
+
+<script>
+import {mapActions} from 'vuex'
+export default {
+  name: 'Feed',
+  methods: {
+    ...mapActions('facebook', [
+    ]),
+    click () {
+    }
+  }
+}
+</script>
